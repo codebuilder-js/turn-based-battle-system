d7ea6d5f4237fc1b8f044d7e9f40d6617953f5c2
Rafael Alves <codebuilder.js@gmail.com>
2025-02-04 14:53:49 -0300

    Create TurnBasedBattleSystem.cs main file

---
+++ TurnBasedBattleSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TurnBasedBattleSystem {
+    static void Main() {
+        Unit player = new Unit(100, 20, 12, "Player");
+        Unit enemy = new Unit(75, 10, 7, "Enemy Mage");
+        Random random = new Random();
+        
+        while(!player.IsDead && !enemy.IsDead) {
+            Console.WriteLine(player.UnitName + " HP = " + player.Hp + " / " + enemy.UnitName + " Hp = " + enemy.Hp);
+            Console.Write("Player turn! What will you do: ");
+            string choice = Console.ReadLine();
+            
+            if(choice == "a")
+                player.Attack(enemy);
+            else
+                player.Heal();
+                
+            if(player.IsDead || enemy.IsDead) break;
+                
+            Console.WriteLine(player.UnitName + " HP = " + player.Hp + " / " + enemy.UnitName + " Hp = " + enemy.Hp);
+            
+            Console.WriteLine("Enemy turn!");
+            
+            int rand = random.Next(0, 2);
+            
+            if(rand == 0)
+                enemy.Attack(player);
+            else
+                enemy.Heal();
+        }
+    }
+}
